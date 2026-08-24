1cfa0e58c0309906c79cffff8c3806fbf9a88e75
LepkoQQ <miha@lepko.net>
2026-08-24 13:06:19 +0200

    add action workflows, add requirements and fix settings

--- mocljudem/mocljudem/settings/dev.py
+++ mocljudem/mocljudem/settings/dev.py
@@ -10,9 +10,3 @@ SECRET_KEY = "django-insecure-!nq9q83-v2h^#!=boz*pys%+5ot)8102-w4g_8g2c*hlkb!$!r
 ALLOWED_HOSTS = ["*"]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-
-
-try:
-    from .local import *
-except ImportError:
-    pass
--- mocljudem/mocljudem/settings/production.py
+++ mocljudem/mocljudem/settings/production.py
@@ -1,16 +1,28 @@
 from .base import *
 
-DEBUG = False
+DEBUG = bool(os.getenv("DJANGO_DEBUG", False))
 
-# ManifestStaticFilesStorage is recommended in production, to prevent
-# outdated JavaScript / CSS assets being served from cache
-# (e.g. after a Wagtail upgrade).
-# See https://docs.djangoproject.com/en/6.1/ref/contrib/staticfiles/#manifeststaticfilesstorage
-STORAGES["staticfiles"][
-    "BACKEND"
-] = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "<TODO>")
 
-try:
-    from .local import *
-except ImportError:
-    pass
+ALLOWED_HOSTS = ["mocljudem.si"]
+CSRF_TRUSTED_ORIGINS = ["https://mocljudem.si"]
+
+STATIC_ROOT = os.getenv("DJANGO_STATIC_ROOT", BASE_DIR / "static")
+STATIC_URL = os.getenv("DJANGO_STATIC_URL_BASE", "/static/")
+
+if os.getenv("DJANGO_ENABLE_S3", False):
+    STORAGES["default"] = {
+        "BACKEND": "storages.backends.s3.S3Storage",
+    }
+    AWS_ACCESS_KEY_ID = os.getenv("DJANGO_AWS_ACCESS_KEY_ID", "<TODO>")
+    AWS_SECRET_ACCESS_KEY = os.getenv("DJANGO_AWS_SECRET_ACCESS_KEY", "<TODO>")
+    AWS_STORAGE_BUCKET_NAME = os.getenv("DJANGO_AWS_STORAGE_BUCKET_NAME", "djnd")
+    AWS_DEFAULT_ACL = "public-read"
+    AWS_QUERYSTRING_AUTH = False
+    AWS_LOCATION = os.getenv("DJANGO_AWS_LOCATION", "mocljudem")
+    AWS_S3_REGION_NAME = os.getenv("DJANGO_AWS_REGION_NAME", "fr-par")
+    AWS_S3_ENDPOINT_URL = os.getenv(
+        "DJANGO_AWS_S3_ENDPOINT_URL", "https://s3.fr-par.scw.cloud"
+    )
+    AWS_S3_SIGNATURE_VERSION = os.getenv("DJANGO_AWS_S3_SIGNATURE_VERSION", "s3v4")
+    AWS_S3_FILE_OVERWRITE = False
