93384acdd3fe25a6ccb192f3eaacd7505d2b9325
LepkoQQ <miha@lepko.net>
2026-08-26 18:57:56 +0200

    expand on status row click

--- mocljudem/mocljudem/static/js/mocljudem.js
+++ mocljudem/mocljudem/static/js/mocljudem.js
@@ -1,21 +1,37 @@
 document.addEventListener("DOMContentLoaded", function () {
   const expandButtons = document.querySelectorAll(".expand-button");
   expandButtons.forEach((button) => {
+    const statusEl = button.closest(".status");
+
     button.addEventListener("click", function () {
-      const statusEl = button.closest(".status");
       const descriptionCol = statusEl.querySelector(".description-col");
       const isExpanded = descriptionCol.getAttribute("aria-hidden") === "false";
       descriptionCol.setAttribute("aria-hidden", isExpanded ? "true" : "false");
       if (isExpanded) {
         statusEl.classList.remove("expanded");
+        statusEl.style.cursor = "pointer";
         button.setAttribute("aria-expanded", "false");
         button.setAttribute("aria-label", "Razširi");
       } else {
         statusEl.classList.add("expanded");
+        statusEl.style.cursor = "";
         button.setAttribute("aria-expanded", "true");
         button.setAttribute("aria-label", "Strni");
       }
     });
+
+    statusEl.addEventListener("click", function (event) {
+      const closestButton = event.target.closest(".expand-button");
+      if (closestButton === button) {
+        return;
+      }
+      const isExpanded = statusEl.classList.contains("expanded");
+      if (isExpanded) {
+        return;
+      }
+      button.click();
+    });
+    statusEl.style.cursor = "pointer";
   });
 
   const newsletterForm = document.querySelector(".newsletter-form");
